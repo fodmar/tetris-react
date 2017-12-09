d55f557a413c3d760b016df88e36ff9d9b0c38d5
fodmar <marcinfoder@gmail.com>
2017-12-09 12:48:38 +0100

    introduce keyhandler

---
+++ js/keyhandler.js
@@ -0,0 +1,33 @@
+class KeyHandler {
+    constructor() {
+        this.KEY_SPACE = 32;
+        this.KEY_LEFT_ARROW =  37;
+        this.KEY_UP_ARROW = 38;
+        this.KEY_RIGHT_ARROW = 39;
+        this.KEY_DOWN_ARROW = 40;
+        this.KEY_P = 80;
+    }
+    
+    handle(e) {
+        if (this.keys.indexOf(e.keyCode) > -1) {
+            this.callback(e);
+        }
+    }
+    
+    register(keys, callback) {
+        this.unregisterAll();
+        
+        this.keys = keys;
+        this.callback = callback;
+        
+        document.addEventListener("keydown", this.handle.bind(this));
+    }
+    
+    unregisterAll() {
+        document.removeEventListener("keydown", this.handle);
+    }
+    
+    getTetrisKeys() {
+        return [this.KEY_SPACE, this.KEY_LEFT_ARROW, this.KEY_UP_ARROW, this.KEY_RIGHT_ARROW, this.KEY_DOWN_ARROW, this.KEY_P];
+    }
+}
--- js/tetris.js
+++ js/tetris.js
@@ -5,12 +5,18 @@ class Tetris extends React.Component {
     
     componentDidMount() {
         this.props.timer.start(this.run.bind(this), this.props.interval);
+        this.props.keyhandler.register(this.props.keyhandler.getTetrisKeys(), this.handleKey.bind(this));
     }
 
     componentWillUnmount() {
         this.props.timer.stop();
+        this.props.keyhandler.unregisterAll();
     }
 
+    handleKey(e) {
+        console.log(e);
+    }
+    
     run() {
         this.setState({});
     }
